56e4719890ca0890b346c66da420c72ac81040c8
pak comp <git>
2022-09-01 01:05:23 +0500

    snakc bar

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,53 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Snack Bar GetX',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Snack Bar GetX'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Get.snackbar(
+                      'SnackBar', 'Snack bar message',
+                      snackPosition: SnackPosition.BOTTOM,
+                      colorText: Colors.white,
+                      backgroundColor: Colors.black,
+                      borderRadius: 20,
+                      margin: const EdgeInsets.all(10),
+                      //maxWidth: 100,
+                      animationDuration: const Duration(milliseconds: 3000),
+                      backgroundGradient: const LinearGradient(
+                          colors: [Colors.red, Colors.green]),
+                      borderColor: Colors.blue,
+                      borderWidth: 3,
+                      boxShadows: [
+                        const BoxShadow(
+                          color: Colors.white30,
+                          blurRadius: 8,
+                          spreadRadius: 20,
+                          offset: Offset(30, 50),
+                        )
+                      ],
+                      forwardAnimationCurve: Curves.bounceInOut,
+                      duration: const Duration(milliseconds: 8000),
+                      isDismissible: true,
+                      icon: const Icon(Icons.accessibility_outlined, color: Colors.white30,),
+                      shouldIconPulse: false,
+                      leftBarIndicatorColor: Colors.pink,
+                      mainButton: TextButton(onPressed: (){}, child:const Text('Retry')),
+                      overlayBlur: 5,
+                    );
+                  },
+                  child: const Text('Show Snack')),
+            ],
+          ),
+        ),
       ),
     );
   }
